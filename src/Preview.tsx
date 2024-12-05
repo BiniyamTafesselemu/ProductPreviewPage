0d018678ee7cccb352f3e892f47435c09c267bcb
BiniyamTafesse <biniyamtafesse228@gmail.com>
2024-12-05 11:24:13 +0300

    mobileresponsive

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,5 +1,5 @@
 import productdesktop from './assets/productdesktop.jpg'; 
-import iconCart from './assets/iconCart.svg' 
+import iconCart from './assets/iconCart.svg'; 
 import productmobile from './assets/productmobile.jpg'; 
 
 const Preview = () => {  
@@ -15,24 +15,30 @@ const Preview = () => {
                     <img   
                         src={productmobile}   
                         alt="perfume"   
-                        className="block sm:hidden w-full h-full object-cover rounded-t-2xl" // Show on smaller screens  
+                        className="block sm:hidden w-full h-full object-cover" // Show on smaller screens  
                     /> 
                 </div>  
                 <div className="w-full sm:w-1/2 rounded-3xl flex flex-col items-center justify-start py-5 sm:py-10"> 
                     <div className="flex flex-col w-full px-5 sm:px-10">
                         <h1 className="text-2xl font-montserrat text-gray-500">PERFUME</h1>
-                        <h1 className="text-4xl sm:text-5xl font-bold mt-4 font-fraunces">Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
-                        <p className="mt-6 font-montserrat">
+                        <h1 className="hidden sm:block text-2xl sm:text-5xl font-bold mt-[30px] font-fraunces sm:mb-10">Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
+                        <h1 className="block sm:hidden text-4xl font-bold mt-[20px] font-fraunces">Gabrielle Essence<br /> Eau De Parfum</h1>
+                        <p className="hidden sm:block mt-[-5px] font-montserrat text-gray-500">
                             A floral, solar and voluptuous<br/>
                             interpretation composed by<br/>
                             Olivier PolgePerfumer-Creator<br/>
                             for the House of CHANEL
                         </p>
-                        <div className="flex flex-row mt-8 items-center">
-                            <h1 className="text-green-400 text-4xl sm:text-5xl font-bold">$149.99</h1>
+                        <p className="block sm:hidden mt-[20px] font-montserrat text-gray-500">
+                            A floral, solar and voluptuous<br/>
+                            interpretation composed by Olivier<br/>PolgePerfumer-Creator
+                            for the House of<br/>CHANEL
+                        </p>
+                        <div className="flex flex-row mt-[25px] items-center">
+                            <h1 className="text-cyan text-4xl sm:text-5xl font-bold font-fraunces">$149.99</h1>
                             <p className="line-through ml-4 text-gray-400">$169.99</p>
                         </div>
-                        <div className="flex flex-row bg-cyan w-full mt-8 h-12 rounded-2xl items-center justify-center hover:bg-darkblue transition-colors">
+                        <div className="flex flex-row bg-cyan w-full mt-[45px] h-[60px] rounded-2xl items-center justify-center hover:bg-darkblue transition-colors">
                             <img className="w-5 h-5" src={iconCart} alt="cart"/>
                             <button className="ml-2 text-white font-bold">Add to cart</button>
                         </div>
